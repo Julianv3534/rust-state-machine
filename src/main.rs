831d473f1a44d4076fafd817b189e3de1fee5cfa
Julianv3534 <julianv3534@gmail.com>
2026-03-10 14:44:49 -0300

    feat: enhance Runtime and System Pallet with transaction management and public method exposure

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,18 @@ impl Runtime {
 }
 
 fn main() {
-	println!("Hello, world!");
+	let mut runtime = Runtime::new();
+	runtime.balances.set_balance(&"alice".to_string(), 100);
+
+	runtime.system.inc_block_number();
+	assert_eq!(runtime.system.block_number(), 1);
+
+	// first transaction
+	runtime.system.inc_account_nonce("alice");
+	let _res = runtime.balances.transfer("alice".to_string(), "bob".to_string(), 30).map_err(|e| eprintln!("{e}"));
+
+	// second transaction
+	runtime.system.inc_account_nonce("alice");
+	let _res = runtime.balances.transfer("alice".to_string(), "charlie".to_string(), 20).map_err(|e| eprintln!("{e}"));
 }
+
--- src/system.rs
+++ src/system.rs
@@ -11,7 +11,7 @@ impl Pallet {
         Self {block_number: 0, nonce: BTreeMap::new()}
 	}
 
-    fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> u32 {
         self.block_number
     }
 
@@ -19,15 +19,15 @@ impl Pallet {
     //and a new block is generated every 6 seconds,
     //it would take over 800 years for an overflow to occur.
 
-    fn inc_block_number(&mut self) {
+    pub fn inc_block_number(&mut self) {
         self.block_number += 1;
     }
 
-    fn account_nonce(&self, account: &str) -> u32 {
+    pub fn account_nonce(&self, account: &str) -> u32 {
         *self.nonce.get(account).unwrap_or(&0)
     }
 
-    fn inc_account_nonce(&mut self, account: &str) {
+    pub fn inc_account_nonce(&mut self, account: &str) {
         let nonce = self.nonce.entry(account.to_string()).or_insert(0);
         *nonce += 1;
     }
