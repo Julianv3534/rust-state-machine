586daf51a0ff18dc0a56dcc70eb8427b6f6c25d1
Julianv3534 <julianv3534@gmail.com>
2026-03-09 23:23:00 -0300

    refactor: improve Pallet struct and add README file

--- src/balances.rs
+++ src/balances.rs
@@ -1,12 +1,22 @@
 use std::collections::BTreeMap;
 
 pub struct Pallet {
-    balances: BTreeMap<String, u128>,
+	//Why BTreeMap and not HashMap?
+	balances: BTreeMap<String, u128>,
 }
 
-impl Pallet{
+impl Pallet {
+	pub fn new() -> Self {
+		Self { balances: BTreeMap::new() }
+	}
 
-    pub fn new() -> Self {
-        Self { balances: BTreeMap::new() }
-    }
+	// can be done with &str and who.to_string() instead of String and clone, but for simplicity we
+	// will use String.
+	pub fn set_balance(&mut self, who: &String, amount: u128) {
+		self.balances.insert(who.clone(), amount);
+	}
+
+	pub fn balance(&self, who: &String) -> u128 {
+		*self.balances.get(who).unwrap_or(&0)
+	}
 }
