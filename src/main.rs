4758cd67c1084f3ee41c2cd482dae321de82f7b1
julianv3534 <julianv3534@gmail.com>
2026-03-14 23:26:52 -0300

    feat: implement block execution and dispatch mechanism in Runtime

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod balances;
 mod support;
 mod system;
 
+use crate::support::Dispatch;
+use types::{AccountId, Balance, BlockNumber, Nonce};
+
 mod types {
 	pub type AccountId = String;
 	pub type Balance = u128;
@@ -16,8 +19,6 @@ pub enum RuntimeCall {
 	Transfer { to: types::AccountId, amount: types::Balance },
 }
 
-use types::{AccountId, Balance, BlockNumber, Nonce};
-
 #[derive(Debug)]
 struct RuntimeConfig;
 
@@ -31,10 +32,11 @@ impl balances::Config for RuntimeConfig {
 	type Balance = Balance;
 }
 
+
 #[derive(Debug)]
 pub struct Runtime {
 	system: system::Pallet<RuntimeConfig>,
-	balances: balances::Pallet<RuntimeConfig>,	
+	balances: balances::Pallet<RuntimeConfig>,
 }
 
 impl Runtime {
@@ -43,6 +45,43 @@ impl Runtime {
 			system: system::Pallet::new(),
 			balances: balances::Pallet::new(),
 		}
+	}
+
+	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+		self.system.inc_block_number();
+		if block.header.block_number != self.system.block_number() {
+			return Err("Block number mismatch.");
+		}
+
+		let block_number = block.header.block_number;
+		for (i, support::Extrinsic { caller, call }) in block.extrinsics.into_iter().enumerate() {
+			self.system.inc_account_nonce(caller.clone());
+			let _res = self.dispatch(caller, call).map_err(|e| {
+				eprintln!(
+					"Extrinsic Error\n\tBlock Number: {}\n\tExtrinsic Number: {}\n\tError: {}",
+					block_number, i, e
+				)
+			});
+		}
+
+		Ok(())
+	}
+}
+
+impl crate::support::Dispatch for Runtime {
+	type Caller = AccountId;
+	type Call = RuntimeCall;
+	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
+	fn dispatch(
+		&mut self,
+		caller: Self::Caller,
+		runtime_call: Self::Call,
+	) -> support::DispatchResult {
+		unimplemented!();
 	}
 }
 
@@ -61,6 +100,6 @@ fn main() {
 	runtime.system.inc_account_nonce("alice".to_string());
 	let _res = runtime.balances.transfer("alice".to_string(), "charlie".to_string(), 20).map_err(|e| eprintln!("{e}"));
 	
-	println!("{:#?}", runtime);
+	println!("{runtime:#?}");
 }
 
