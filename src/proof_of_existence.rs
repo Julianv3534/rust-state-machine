fdaac67f73f162fd14684d4a7c6756dfc485c401
julianv3534 <julianv3534@gmail.com>
2026-03-16 10:23:31 -0300

    feat: add proof_of_existence module for content claims management

---
+++ src/proof_of_existence.rs
@@ -0,0 +1,23 @@
+use core::fmt::Debug;
+use std::collections::BTreeMap;
+
+pub trait Config: crate::system::Config {
+	/// The type which represents the content that can be claimed using this pallet.
+	/// Could be the content directly as bytes, or better yet the hash of that content.
+	/// We leave that decision to the runtime developer.
+	type Content: Debug + Ord;
+}
+
+/// This is a simple module that allows accounts to claim existence of some data.
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+	/// A simple storage map from content to the owner of that content.
+	/// Accounts can make multiple different claims, but each claim can only have one owner.
+    claims: BTreeMap<T::Content, T::AccountId>,
+}
+
+impl<T: Config> Pallet<T> {
+	pub fn new() -> Self {
+		Self { claims: BTreeMap::new() }
+	}
+}
